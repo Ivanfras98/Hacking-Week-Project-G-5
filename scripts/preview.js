6d41fe7cafc396f9bbbc5231defe08b47e650064
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-08-04 19:46:50 +0200

    Hidden scrollbar when not needed

--- scripts/preview.js
+++ scripts/preview.js
@@ -8,6 +8,7 @@ const btnTable = document.querySelector("#table-btn");
 
 btn.addEventListener("click", () => {
   document.querySelector("main").style.visibility = "visible";
+  document.querySelector("main").style.overflowY = "hidden";
   document.querySelector(".chart").classList.add("visible");
   document.querySelector(".tabella").classList.add("visible");
   document.getElementById("calendar").style.display = "flex";
@@ -15,6 +16,7 @@ btn.addEventListener("click", () => {
 
 btnTable.addEventListener("click", () => {
   document.querySelector("main").style.visibility = "visible";
+  document.querySelector("main").style.overflowY = "scroll";
   document.querySelector(".chart").classList.add("visible");
   document.getElementById("calendar").style.display = "none";
   document.querySelector(".tabella").classList.remove("visible");
@@ -22,6 +24,7 @@ btnTable.addEventListener("click", () => {
 
 btnChart.addEventListener("click", () => {
   document.querySelector("main").style.visibility = "visible";
+  document.querySelector("main").style.overflowY = "hidden";
   document.querySelector(".tabella").classList.add("visible");
   document.getElementById("calendar").style.display = "none";
   document.querySelector(".chart").classList.remove("visible");
